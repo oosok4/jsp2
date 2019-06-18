f20f17cee624e9cd8d8a1a0e34a5fa0f9b9d8b0d
oosok4 <oosok4@naver.com>
2019-06-18 19:58:48 +0900

    pageContext 주석처리

--- src/main/java/kr/or/ddit/db/text/controller/createText.java
+++ src/main/java/kr/or/ddit/db/text/controller/createText.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import javax.servlet.jsp.PageContext;
+//import javax.servlet.jsp.PageContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
